1469c48a85e610a2e643c0fc7ba252bb119c2c9a
Laura-Eliise <laura-eliise@marrandi.ee>
2021-11-15 12:13:46 +0200

    added formula and subtraction functions

--- path/path.go
+++ path/path.go
@@ -13,6 +13,7 @@ var (
 // func Path() [][]string {
 func main() {
 	// 0 - 1/2/3 - 4 - 5
+	ants := 10
 	anthill["0"] = []string{"start", "1", "2", "3"}
 	anthill["1"] = []string{"middle", "0", "4"}
 	anthill["2"] = []string{"middle", "0", "4"}
@@ -27,7 +28,7 @@ func main() {
 		}
 	}
 
-	fmt.Println(filter())
+	fmt.Println(filter(ants))
 }
 
 // findWay takes an anthill layout and gives us all the possible paths to the end
@@ -55,12 +56,13 @@ func findWay(room string, way []string) {
 }
 
 // takes two roads, eachs if each room is unique
-func filter() [][]string {
+func filter(ants int) ([][]string, []int) {
+	var rightDistribution []int
 	var rightWay [][]string
 
 	// if we only found one path, that's that
 	if len(paths) == 1 {
-		return paths
+		return paths, []int{ants}
 	}
 
 	// sort the list, the shortest path at the front
@@ -85,19 +87,15 @@ func filter() [][]string {
 		way = findBranchingPaths(short, way)
 
 		if len(rightWay) == 0 {
+			_, rightDistribution = moveAnts(way)
 			rightWay = way
 		} else {
-			// 1. tuleb vaadata mitu branchi kummaski on
-			// 2. Kui palju sipelgaid on 
+			rightWay, rightDistribution = compare(rightWay, way, ants)
 		}
 	}
 
-	return rightWay
+	return rightWay, rightDistribution
 }
-// if number of rouths on < sipelgat arv - kasuta kõiki võimalikke teid ja kui sipelgate arv on sama või väiksem kui path siis kasutage kõige lühemat
-
-// 2 	 = 2 liigutust - 1 = 5 liigutust - 4 = 6 liigutust - 5 = 7 liigutust - 6
-// 5 - 3 = 5 liigutust - 3 = 5 liigutust - 3 = 6 liigutust - 5 = 7 liigutust - 7
 
 func findBranchingPaths(short []string, way [][]string) [][]string {
 	// in both middles, start and end get cut off for all roads share that
@@ -134,3 +132,156 @@ func findBranchingPaths(short []string, way [][]string) [][]string {
 
 	return way
 }
+
+ // moveAnts takes in a given way and the number of ants
+ // and return how many ants finished base on how long
+ // it'll take for the ant on the longest path to get home
+ // and how those ant are divided to each road.
+func moveAnts(way [][]string) (int, []int) {
+	if len(way) == 1 {
+		return 1, []int{1}
+	}
+
+	var list []int 						// the number of ants that are sent down each path
+	var antsfinished int				// ants that finish by x number of moves
+	longestPath := len(way[len(way)-1]) // length of the longest path
+	antsfinished++
+
+	for i := 0; i < len(way)-2; i++ {
+		antsfinished += len(way[i]) - longestPath
+		list = append(list, len(way[i]) - longestPath) 
+	}
+
+	list = append(list, 1)
+
+	return antsfinished, list
+}
+
+// a formula to send the right amount of ants down each path, return the distribution and the move count
+func formula(option [][]string, ants int) ([]int, int) {
+	finished, distribution := moveAnts(option)
+	roadCount := len(option)
+
+	// if the way we distributed the ants is greater than the amount of ants we have...
+	// ...then we're fucked...
+	// if it's the same then we got off easy.
+	if finished > ants {
+		moves := len(option[len(option)-1])
+		return subtraction(ants, finished, distribution, moves)
+	} else if finished == ants {
+		return distribution, len(option[len(option)-1])
+	}
+	
+	// start : we send out the beginning path of unevenly distributed ants
+	moves := len(option[len(option)-1])
+	ants = ants-finished
+
+	// middle/end : now that the uneven part is done then we 
+	moves += ants/roadCount
+	ants = ants%roadCount
+	for i := range distribution {
+		distribution[i] += ants/roadCount
+	}
+
+	// end : if theres still some ants lingering then we do one extra move for them
+	if ants > 0 {
+		moves++
+		for i := range distribution {
+			distribution[i] += 1
+			ants--
+			if ants == 0 {
+				break
+			}
+		}
+	}
+
+	return distribution, moves
+}
+
+// if the way we distributed the ants is greater than the amount of ants we have
+// then we start subtracting them from te roads
+func subtraction(ants int, finished int, distribution[]int, moves int) ([]int, int) {
+	for i := 0; i < len(distribution); i++ {
+		distribution[i] -= 1
+		finished--
+
+		// need to make it soo that the option also loses it's value in that case...
+		if distribution[i] == 0 {
+			temp := distribution[:i]
+			temp = append(temp, distribution[i+1:]...)
+			distribution = temp
+			i--
+		}
+
+		if finished == ants {
+			if i == len(distribution) - 1 {
+				moves--
+			}
+			return distribution, moves
+		}
+	}
+	moves--
+	return subtraction(ants, finished, distribution, moves)
+}
+
+	/*
+	ants = 100 - 3 = 97
+	moves = 0 + 5 = 5
+
+	[97 / 2 = 48] 
+	moves = 5 + 48 = 53
+	ants = 97 % 2 = 2
+
+	if ants(2) > 0 {
+		moves++
+	}
+
+	to find the final amount of moves that's needed to finish.
+
+	first we need to find how many ants will finish by the time the longest ant takes to finish
+	we save these as 2 moves and 3 finished. 
+	
+	firstfinish = 3, longest road length = 5
+	ants = 100 moves = 0 
+
+	100 - 3 = 97
+	moves = 0 + 5 = 5
+
+	ants = 97 moves = 5
+
+	now we have the base number of ants we will start working with. After the first iteration of ants is home,
+	then we just need to send one after the other down each path, meaning, if we divide the base number of ants 
+	we got with how many roads we have, we have the middle part of the moving process done.
+
+	97 / 2 = 48 
+	moves = 5 + 48 = 53
+	ants = 97 - 48 * 2 = 97-96 = 2
+	*/
+
+	// if number of rouths on < sipelgat arv - kasuta kõiki võimalikke teid ja kui sipelgate arv on sama või väiksem kui path siis kasutage kõige lühemat
+
+/* 
+
+if 10
+2 	  = 2 liigutust - 1 = 5 liigutust - 4 = 6 liigutust - 5 = 7 liigutust - 6 = 8 liigutust - 7
+5 - 3 = 5 liigutust - 3 = 5 liigutust - 3 = 6 liigutust - 5 = 7 liigutust - 7 = 8 liigutust - 9
+
+100
+
+numberOfAnts - baseFinished
+resultOfPrevious / numpaths 
+
+100 - 3 = 97
+97 / 2 = 48 
+if 97 % 2 > 0 {
+	97 % 2 = 1
+	48 + 1 % 2 + 1 / 2 = 49 
+}
+49 + 5 = 54
+
+100 - 1 = 99
+99 / 1 = 99
+if...
+99 + 2 = 101
+
+*/
